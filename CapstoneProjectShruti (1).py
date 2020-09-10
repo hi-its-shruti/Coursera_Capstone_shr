3f1b04646420a12a182251d66daaab2e1a60fc9c
hi-its-shruti <69518745+hi-its-shruti@users.noreply.github.com>
2020-09-10 11:37:34 +0530

    Add files via upload

---
+++ CapstoneProjectShruti (1).py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## This is submission for week1 assignment of Capston project
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
